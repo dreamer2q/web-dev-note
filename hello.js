c100140a4902585ac6fe45f9ff8ebe0b2edae311
dreamer2q <dreamer22qq@gmail.com>
2020-10-13 01:07:20 +0800

    add books

--- hello.js
+++ hello.js
@@ -40,11 +40,65 @@ for (const c of "123456") {
 }
 
 var global = () => this;
-
 var color = "red";
+// log(global.color);
 
-log(global.color);
 
+let randomVar = [];
+for (let i = 0; i < 10000; i++) {
+    let tmp = Math.random();
+    randomVar.push(tmp);
+}
+
+log(Math.max(...randomVar));
+
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, "1", "2", "3", "4"];
+
+const arrKey = arr.keys();
+const arrVal = arr.values();
+const arrEntr = arr.entries();
+
+
+let person = {
+    name: "Jack",
+    toString() {
+        return "Jack";
+    },
+    toLocalString() {
+        return toString();
+    }
+}
+
+
+log(person);
+
+
+let stack = [];
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(1);
+stack.push(10);
+stack.push(-10);
+
+
+// log(stack.sort((v1, v2) => v1 - v2));
+// log(stack.reverse());
+
+// 缓存
+// TODO 
+const buf = new ArrayBuffer(1024);
+log(buf.byteLength);
+// 使用视图，DataView来进行读写操作
+const view = new DataView(buf);
+
+view.setUint8(0, 0x80);
+view.setUint8(1, 0x01);
+
+log(view.getUint16(0));
+log(view.getUint16(0, true));
 
 
 
